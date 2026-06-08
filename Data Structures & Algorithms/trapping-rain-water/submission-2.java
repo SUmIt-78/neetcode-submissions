df4bcb58959c8e7f90dff1a388abb2e67ca7702f
SUmIt-78 <114091861+SUmIt-78@users.noreply.github.com>
2026-06-08 10:58:02 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int trap(int[] height) {
+        int[] preMax=new int[height.length];
+        int[] sufMax=new int[height.length];
+        int total=0;
+        
+         for(int i=0;i<height.length;i++){
+            preMax[i]= i==0? height[i]: Math.max(preMax[i-1],height[i]);
+         }
+
+         for(int i=height.length-1;i>=0;i--){
+            sufMax[i]=i==height.length-1? height[i]:Math.max(sufMax[i+1],height[i]);
+         }
+
+
+
+        for(int i=0;i<sufMax.length;i++){
+            if(preMax[i]==height[i] || sufMax[i]==height[i])continue;
+            int content=Math.min(preMax[i],sufMax[i])-height[i];
+            total+=content;
+        }
+
+        return total;
+
+
+
+    }
+}
