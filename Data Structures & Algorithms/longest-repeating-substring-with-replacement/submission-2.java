6dea64216cc2fbaa07aa23533a24c85c1b05abc3
SUmIt-78 <114091861+SUmIt-78@users.noreply.github.com>
2026-06-09 10:55:38 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int l=0;
+        int r=0;
+        int[] freq=new int[27];
+        int maxFreq=0;
+        int maxLen=0;
+
+        while(r<s.length()){
+
+            freq[s.charAt(r)-'A']++;
+            maxFreq=Math.max(maxFreq,freq[s.charAt(r)-'A']);
+            while((r-l+1)>maxFreq+k){
+                freq[s.charAt(l)-'A']--;
+                for(int fr:freq){
+                    maxFreq=Math.max(maxFreq,fr);
+                }
+                l++;
+            }
+
+            maxLen=Math.max(maxLen,(r-l+1));
+            r++;
+        }
+        return maxLen;
+    }
+}
