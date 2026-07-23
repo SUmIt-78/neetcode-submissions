3595d583439c6083672fe7144c1dbe4e8f28a51f
Sumit Raut <114091861+SUmIt-78@users.noreply.github.com>
2026-07-23 11:00:24 +0530

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.java
@@ -0,0 +1,32 @@
+class Solution {
+     public void subsetsWithDup(int[] nums, List<List<Integer>> res, List<Integer> curr, int indx, boolean[] visited) {
+        if(indx==nums.length){
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+    while(indx<nums.length && indx>0 && nums[indx-1]==nums[indx] && !visited[indx-1])indx++;
+       if(indx==nums.length){
+       subsetsWithDup(nums,res,curr,indx,visited);
+       return;
+       }
+        visited[indx]=true;
+        curr.add(nums[indx]);
+        subsetsWithDup(nums,res,curr,indx+1,visited);
+        visited[indx]=false;
+        curr.remove(curr.size()-1);
+        subsetsWithDup(nums,res,curr,indx+1,visited);
+    }
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> curr=new ArrayList<>();
+        boolean[] visited=new boolean[nums.length];
+        subsetsWithDup(nums,res,curr,0,visited);
+        List<Integer> empty=new ArrayList<>();
+        if(!res.contains(empty)){
+            res.add(empty);
+        }
+        return res;
+    }
+}
