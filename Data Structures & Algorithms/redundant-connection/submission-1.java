4d3d016739b69edd6171bf42a93f97caa3890904
Sumit Raut <114091861+SUmIt-78@users.noreply.github.com>
2026-08-02 00:04:11 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.java
@@ -0,0 +1,38 @@
+
+class DSU{
+    int[] parents;
+
+   public DSU(){
+    parents=new int[101];
+    for(int i=0;i<101;i++){
+        parents[i]=i;
+    }
+   }
+
+   public int find(int x){
+    if(parents[x]==x)return x;
+    return parents[x]=find(parents[x]);
+   }
+
+   public boolean union(int u,int v){
+    int pu=find(u);
+    int pv=find(v);
+    if(pu==pv)return false;
+    parents[pu]=pv;
+    return true;
+   }
+
+
+}
+
+
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        DSU dsu=new DSU();
+        int[] res=null;
+        for(int[] edge:edges){
+            if(!dsu.union(edge[0],edge[1]))res=edge;
+        }
+        return res;
+    }
+}
